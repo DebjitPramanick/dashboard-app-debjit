8854c64f13d6cd318125e09ca9c65e8031509871
DebjitPramanick <debjitpramanick1@gmail.com>
2025-02-05 01:19:01 +0530

    Remove unnecessary components

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,3 @@
 export { default as Box } from "./Box";
 export { default as Flex } from "./Flex";
 export { default as Text } from "./Text";
-export { default as Input } from "./Input";
-export { default as Select } from "./Select";
